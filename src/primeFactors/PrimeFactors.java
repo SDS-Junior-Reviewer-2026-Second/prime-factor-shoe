5dbd05496e1c93a6f311f4e3f5f8e50c119f0be1
박시우 <pswudo@gmail.com>
2026-09-18 10:30:47 +0900

    feat: support prime factorization of 3

--- src/primeFactors/PrimeFactors.java
+++ src/primeFactors/PrimeFactors.java
@@ -1,5 +1,18 @@
 package primeFactors;
 
+import java.util.List;
+
 public class PrimeFactors {
 
+    public static List<Integer> generate(int number) {
+        if (number == 2) {
+            return List.of(2);
+        }
+
+        else if (number == 3) {
+            return List.of(3);
+        }
+
+        return List.of();
+    }
 }
--- test/primeFactors/PrimeFactorsTest.java
+++ test/primeFactors/PrimeFactorsTest.java
@@ -1,10 +1,31 @@
 package primeFactors;
 
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
 public class PrimeFactorsTest {
 
+	PrimeFactors primeFactors = new PrimeFactors();
+
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void one() {
+		assertEquals(List.of(), primeFactors.generate(1));
 	}
+
+	@Test
+	public void two() {
+		assertEquals(List.of(2), PrimeFactors.generate(2));
+	}
+
+	@Test
+	public void three() {
+		assertEquals(List.of(3), PrimeFactors.generate(3));
+	}
+
+
 
 }
